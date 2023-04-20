106be4c0e682fb9443eb7154abbfad3272ba245f
David Hudák <61205638+DaveHudiny@users.noreply.github.com>
2023-04-20 10:30:04 +0200

    A lot of experiments

--- cgp_interface.py
+++ cgp_interface.py
@@ -12,7 +12,7 @@ import numpy as np
 import scipy.ndimage as nd
 import pickle as pkl
 import random as random
-from strategies import two_outputs, deterministic
+from strategies import two_outputs, deterministic, no_threshold, three_outputs
 import argparse
 import inspect
 
@@ -54,36 +54,54 @@ class Avg(cgp.OperatorNode):
     _arity = 2
     _def_output = "(x_0 + x_1) / 2"
     _def_output_numpy = "(x_0 + x_1) / 2"
+
+
     
 
 class CGP_interface():
-    def __init__(self, correct, noisy, error_function, seed : int, strategy):
+    def __init__(self, correct, noisy, error_function, seed : int, strategy, iterations):
         self._correct = (correct / 255.0) - 0.5
         self._noisy = (noisy / 255.0) - 0.5
         self._changes = (self._correct != self._noisy)
         self._error_function = error_function
         self.functor = Functor(None, strategy, 2)
         self.population_params = {"n_parents": 2, "seed": seed}
+        self.height, self.width = self._correct.shape
+
+        global size_prod 
+        size_prod = self.height * self.width
+        if strategy == three_outputs:
+            self.n_outputs = 3
+        elif strategy == two_outputs:
+            self.n_outputs = 2
+        else:
+            self.n_outputs = 1
         self.genome_params = {
             "n_inputs": 25,
-            "n_outputs": 2,
+            "n_outputs": self.n_outputs,
             "n_columns": 7,
             "n_rows": 9,
             "levels_back": 7,
             "primitives": (cgp.Add, cgp.Sub, cgp.Mul, cgp.ConstantFloat, cgp.IfElse, DivProtected, 
-                           ConstantOne, ConstantZero, Mini, Identity, AbsSub, Avg),
+                           ConstantOne, ConstantZero, Maxi, Mini, Identity, AbsSub, Avg),
             }
         self.ea_params = {"n_offsprings": 5, "mutation_rate": 0.3, "n_processes": 4}
-        self.evolve_params = {"max_generations": 400}
-
-    def objective(self, individual : cgp.IndividualSingleGenome):
+        self.evolve_params = {"max_generations": iterations}
+    
+    def objective_deterministic(self, individual : cgp.IndividualSingleGenome):
+        self.functor.set_function(individual.to_func())
+        filtered_image = nd.generic_filter(self._noisy, self.functor.work, (5,5))
+        individual.fitness = (self._error_function(self._correct, filtered_image))
+        return individual
+    
+    def objective_two_outputs(self, individual : cgp.IndividualSingleGenome):
+        global size_prod
         self.functor.set_function(individual.to_func())
         filtered_image = nd.generic_filter(self._noisy, self.functor.work, (5,5))
         filtered_pixels = (filtered_image == self._noisy)
         correct_changes = np.sum(filtered_pixels != self._changes)
-        height, width = filtered_pixels.shape
         individual.fitness = (self._error_function(self._correct, filtered_image)
-                              - 0.5 * (correct_changes / (height * width)))
+                              - 0.1 * (correct_changes / (size_prod)))
         return individual
 
 class Functor():
@@ -100,10 +118,10 @@ class Functor():
         return self._strategy(self._function, inputor)
 
 def MSE(img1, img2):
-    height, width = img1.shape
+    global size_prod
     # diff = np.sum(cv2.subtract(img2, img1) ** 2)
-    diff = np.sum((np.asarray(img1) - np.asarray(img2)) ** 2)
-    scaled = ((height*width) - diff) / (height*width)
+    diff = np.sum((img1 - img2) ** 2)
+    scaled = ((size_prod) - diff) / (size_prod)
     return scaled
 
 def load_image(path_correct : str, path_noisy : str):
@@ -132,31 +150,45 @@ def recording_callback(pop):
     global seed
     if iteration == 0:
         history["champion_fitness"].append(seed)
-    if iteration % 20 == 0:
-        history["champion_fitness"].append(pop.champion.fitness)
+    history["champion_fitness"].append(pop.champion.fitness)
     iteration += 1
 
-
-if __name__ == "__main__":
+def parser_init():
     parser = argparse.ArgumentParser(description='Program for cgp algorithm')
-    parser.add_argument("--clean_path", type=str, help="Path to clear image (no noises).", default="./data/tshushima_small.jpg")
-    parser.add_argument("--noisy_path", type=str, help="Path to noisy image.", default="./data/tshushima_small_20percent.jpg")
+    parser.add_argument("--clean_path", type=str, help="Path to clear image (no noises).", default="./data/tshushima_small_60.jpg")
+    parser.add_argument("--noisy_path", type=str, help="Path to noisy image.", default="./data/tshushima_small_15percent.jpg")
     parser.add_argument("--runs", type=int, help="Number of program runs.", default=15)
     parser.add_argument("--result_path", type=str, help="Path to folder for results.", default="./experimenty")
-    parser.add_argument("--two_out_strategy", help="Strategy for two outputs.")
-    parser.add_argument("--deter_strategy", help="Strategy with deterministic detection.")
-    args = parser.parse_args()
-    if args.two_out_strategy is not None:
-        strategy = two_outputs
-        strategy_name = "two_outputs"
-    elif args.deter_strategy is not None:
-        strategy = deterministic
-        strategy_name = "deterministic"
+    parser.add_argument("--strategy", help="Strategy for two outputs.", choices=["two_outputs", "deterministic", "no_threshold", "three_outputs"])
+    parser.add_argument("--iterations", help="Iterations (generations) for each run.", default=400, type=int)
+    return parser
+
+def select_strategy(args):
+    if args.strategy is not None:
+        if args.strategy == "two_outputs":
+            strategy = two_outputs
+            strategy_name = "two_outputs"
+        elif args.strategy == "deterministic":
+            strategy = deterministic
+            strategy_name = "deterministic"
+        elif args.strategy == "no_threshold":
+            strategy = no_threshold
+            strategy_name = "no_threshold"
+        elif args.strategy == "three_outputs":
+            strategy = three_outputs
+            strategy_name = "three_outputs"
     else:
         print("No strategy was selected! Two outputs strategy will be performed!")
         strategy = two_outputs
         strategy_name = "two_outputs"
-    print(args.clean_path)
+    return strategy, strategy_name
+
+if __name__ == "__main__":
+    parser = parser_init()
+    args = parser.parse_args()
+    strategy, strategy_name = select_strategy(args)
+
+    # print(args.clean_path)
     img1, img2 = load_image(args.clean_path, args.noisy_path)
     if img1 is None:
         print("Ilegální obrázek")
@@ -168,14 +200,14 @@ if __name__ == "__main__":
     global seed
     for i in range(args.runs):
         seed = int.from_bytes(os.urandom(4), 'big')
-        interface = CGP_interface(img1, img2, MSE, seed, strategy)
+        interface = CGP_interface(img1, img2, MSE, seed, strategy, args.iterations)
         iteration = 0
         history["champion_fitness"] = []
         # print(interface.objective(np.array([[0.01,0.024,0.01],[0.024,0.9,0.024],[0.01,0.024,0.01]])))
         pop = cgp.Population(**interface.population_params, genome_params=interface.genome_params)
         ea = cgp.ea.MuPlusLambda(**interface.ea_params)
         # try:
-        cgp.evolve(pop, interface.objective, ea, **interface.evolve_params, print_progress=True, callback=recording_callback)
+        cgp.evolve(pop, interface.objective_two_outputs, ea, **interface.evolve_params, print_progress=True, callback=recording_callback)
         # except:
             # with open('ended_randomly.pkl', 'wb') as handle:
             #     pkl.dump(pop.champion, handle, protocol=pkl.HIGHEST_PROTOCOL)
@@ -186,7 +218,11 @@ if __name__ == "__main__":
         img_new = nd.generic_filter((img2 / 255.0) - 0.5, fun.work, (5,5))
             # print(MSE(img1, img_new))
             
-
+        filtered_pixels = (img_new == interface._noisy)
+        correct_changes = np.sum(filtered_pixels != interface._changes)
+        height, width = filtered_pixels.shape
+        if strategy_name == "deterministic":
+            history[1:] -= 0.1 * correct_changes / (height * width)
         print_image(img_new, f"{args.result_path}/{strategy_name}/res{i}.jpg")
         with open(f"{args.result_path}/{strategy_name}/histories.txt", "a") as f:
             f.write(' '.join(str(aux) for aux in history["champion_fitness"]))
--- strategies.py
+++ strategies.py
@@ -5,7 +5,8 @@
 # File = strategies.py
 # Short Description = file containing strategies for project to subject Applied Evolutionary Algorithms
 
-import inspect
+import numpy as np
+where = np.array([6, 7, 8, 11, 12, 13, 16, 17, 18])
 
 def two_outputs(func, inputor):
     changeif, new_value = func(*inputor)
@@ -16,7 +17,21 @@ def two_outputs(func, inputor):
     
 def deterministic(func, inputor):
     # filter implementation
-    # if deterministic_says_yes():
+    
+    neighbourhood = inputor[where]
+    mean = np.mean(neighbourhood)
+    std = np.std(neighbourhood)
+    if np.abs(mean - neighbourhood[4]) > std:
         return func(*inputor)
-    # else:
+    else:
         return inputor[12]
+    
+def no_threshold(func, inputor):
+    return func(*inputor)
+
+def three_outputs(func, inputor):
+    out1, out2, out3 = func(*inputor)
+    if out1 >= 0:
+        return out2
+    else:
+        return out3
