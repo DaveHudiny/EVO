1692eba41bd2f31347de9cf91461eaf75894eb10
David Hudák <61205638+DaveHudiny@users.noreply.github.com>
2023-04-21 10:32:02 +0200

    Moved files

--- plots.py
+++ plots.py
@@ -1,3 +1,9 @@
+# Author = David Hudak
+# Login = xhudak03
+# Subject = EVO
+# Year = 2022/2023
+# Short Description = main file of project to subject Applied Evolutionary Algorithms
+
 import seaborn as sea
 
 
